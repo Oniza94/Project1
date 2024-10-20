824da24ae316198c17fa64ef5db319354003ff8f
Oniza Fazal <160499036+Oniza94@users.noreply.github.com>
2024-10-20 09:37:46 +0500

    Add files via upload
    ok

---
+++ page.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/header"
+import Footer from "./components/footer"
+import About from "./about-us/page"
+
+
+export default function projects() {
+  return(
+    <>
+     <Header/> 
+     <Footer/>
+
+    </>
+      )
+}
+
